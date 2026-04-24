682918e8094bd83d05b8329523d393f642c67176
squashtbh <saxena.aakash05@gmail.com>
2026-04-24 19:09:58 +0000

    correcting the hosting file again

--- self_paced_courses_1_mlops/hosting/hosting.py
+++ self_paced_courses_1_mlops/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="self_paced_courses_1_mlops/deployment",
-    repo_id="squash2205/PIMA-Diabetes-Prediction"                                         # enter the Hugging Face username here
+    repo_id="squash2205/PIMA-Diabetes-Prediction",                                        # enter the Hugging Face username here
     repo_type="space",
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
